99696e17d756cee4036dbc2adc46987c14e9bd00
liuli <2283582156@qq.com>
2017-05-26 13:47:49 +0800

    讲话稿

--- src/springMvc/indexController.java
+++ src/springMvc/indexController.java
@@ -5,7 +5,12 @@ public class indexController {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 String dd ="testImport";
+		 int dfd=1;
 		 
 	}
-
+	public  void uiu(String[] args) {
+		// TODO Auto-generated method stub
+		 String dd ="testImport";
+		 
+	}
 }
